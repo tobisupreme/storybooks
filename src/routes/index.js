5a0c68bae1b86bd6a83743d6ca9a17d6d26b2f3e
Tobi Balogun <tobi.balo@gmail.com>
2022-11-13 12:01:46 +0100

    feat(stories): add story model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const Story = require('../models/Story')
 
 /**
  * @description Login/Landing page
  * @route GET /
  */
-
 router.route('/').get(ensureGuest, (req, res) => {
   res.render('login', { layout: 'login' })
 })
@@ -14,10 +14,20 @@ router.route('/').get(ensureGuest, (req, res) => {
  * @description Dashboard
  * @route GET /dashboard
  */
-
-router.route('/dashboard').get(ensureAuth, (req, res) => {
-  console.log(req.user)
-  res.render('dashboard')
+router.route('/dashboard').get(ensureAuth, async (req, res) => {
+  const { firstName, lastName, image } = req.user
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard', {
+      firstName,
+      lastName,
+      image,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 module.exports = router
